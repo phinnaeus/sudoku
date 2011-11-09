1d3aefa9d507bb257c14aaf0c4e89ac1455fbf97
Tyler Davis <tyler.davis@gmail.com>
2011-11-09 18:53:34 -0500

    fixed autotab skipping cells on bad characters

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
         });
     });
 
-    $("#puzzle input").autotab_magic().autotab_filter("numeric")
+    $("#puzzle input").autotab_filter("numeric").autotab_magic();
 	
 	//this function attached focus and blur events with input elements
 	var addFocusAndBlur = function($input, $val){
