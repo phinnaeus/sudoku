81fab1577ab2d6b850fbaef3eb173f5aacc14e97
Tyler Davis <tyler.davis@gmail.com>
2011-11-09 22:49:54 -0500

    started redesign

--- js/script.js
+++ js/script.js
@@ -5,50 +5,6 @@ $(document).ready(function(){
 
     jQuery.easing.def = 'easeInExpo';
 
-	function hideOverlay() {
-        $(".tooltip").hide();
-        $("#overlayBar").animate({
-            top: '-16%',
-        }, 500, function() {
-            $("#overlay").fadeOut()
-            $("#overlayBar").hide();
-            $(".hidden").animate({
-                opacity: '1',
-            });
-        });
-    }
-
-    $(".overlayButton").bind('mouseenter', function() {
-        var p = $(this).position();
-        var t = $(this).children(".tooltip");
-        t.css({
-            'top' : p.top + 60,
-            'left' : p.left - 20
-        });
-        t.fadeIn('fast');
-    });
-    
-
-    $(".overlayButton").bind('mouseleave', function() {        
-        var t = $(this).children(".tooltip");
-        t.delay(500).fadeOut('fast');
-    });
-
-    $("#solverButton").click(function() {
-        hideOverlay();
-        $("#rightMenu").show().animate({
-            right: '-2px',
-        });
-        randomShittyPuzzle();
-    });
-
-    $("#creatorButton").click(function() {
-        hideOverlay();
-        $("#leftMenu").show().animate({
-            left: '-2px',
-        });
-    });
-
     $("#puzzle input").autotab_filter("numeric").autotab_magic();
 	
 	//this function attached focus and blur events with input elements
@@ -66,7 +22,4 @@ $(document).ready(function(){
 	// example code to attach the events
 	//addFocusAndBlur(jQuery('#name'),'Name');
 	
-           /* $("#leftMenu").show().animate({
-                left: '-2px',
-            });*/
 });
