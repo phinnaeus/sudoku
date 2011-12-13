f460073a063b86cca013e4a4b2b5fb7ff7818b35
Tyler Davis <tyler.davis@gmail.com>
2011-12-13 17:32:12 -0500

    starting difficulty selection work

--- js/sudoku.js
+++ js/sudoku.js
@@ -912,6 +912,27 @@ Storage.prototype.getObject = function(key, value) {
     return JSON.parse(this.getItem(key));
 }
 
+function getDifficulty() {
+    return localStorage.getItem("davistm+coleycj_diff");
+}
+
+function setDifficulty(diff) {
+    try {
+        localStorage.setItem("davistm+coleycj_diff",diff);
+    } catch (e) {
+        if (e == QUOTA_EXCEEDED_ERR) {
+            alert("well, fuck. we ran out of room.");
+        } else alert(e);
+    }
+}
+
+function displayDifficulty(diff) {
+    $(".difficultyOrbs").removeClass("on").addClass("off");
+    for (; diff>=1; diff--) {
+        $("orb" + diff).addClass("on");
+    }
+}
+
 // test puzzles
 
 function grabPuzzle(difficulty) {
