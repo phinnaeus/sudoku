baa7c4fbe6b54e009c831f7626dfd2a1411a4322
Tyler Davis <tyler.davis@gmail.com>
2011-12-13 21:12:07 -0500

    fixed tooltips, fixed default difficulty

--- js/script.js
+++ js/script.js
@@ -24,26 +24,11 @@ $(document).ready(function(){
         updateButtons();
     });
 
-    $("#leftMenu button[title]").tooltip({position: "center left", effect: "slide", direction: "left"});
-    $("#rightMenu button[title]").tooltip({position: "center right", effect: "slide", direction: "right"});
+    $("#leftMenu button[title]").tooltip({tipClass: "tooltipLeft", position: "center left", effect: "slide", direction: "left"});
+    $("#rightMenu button[title]").tooltip({tipClass: "tooltipRight", position: "center right", effect: "slide", direction: "right"});
     $(".difficultyOrbs[title]").tooltip({position: "top center", effect: "slide"});
 
-    if (getDifficulty() == "") setDifficulty(4);
+    if (getDifficulty() == null) setDifficulty(4);
     else displayDifficulty(getDifficulty());
-	
-    //this function attached focus and blur events with input elements
-	var addFocusAndBlur = function($input, $val){
-		
-		$input.focus(function(){
-			if (this.value == $val) {this.value = '';}
-		});
-		
-		$input.blur(function(){
-			if (this.value == '') {this.value = $val;}
-		});
-	}
-
-	// example code to attach the events
-	//addFocusAndBlur(jQuery('#name'),'Name');
 	
 });
